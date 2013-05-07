d3f0d65c0a42c7b762c87647ec899339b5f178d8
Marie Hogebrandt <marie.hogebrandt@pineberry.com>
2013-05-07 09:01:34 +0200

    Moving mediaqueries to main.js, making ornaments smaller around titles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,13 @@
-console.log('\'Allo \'Allo!');
+$(function () {
+    'use strict';
+    // By binding them to both load and resize, it works on the window
+    $(window).on('load resize', function () {
+        var mqSupport = Modernizr.mq('only all');
 
+        if (!mqSupport || Modernizr.mq('(min-width: 37.5em)')) {
+            $('.thumbnail').addClass('fancybox');
+        } else {
+            $('.thumbnail').removeClass('fancybox');
+        }
+    });
+});
