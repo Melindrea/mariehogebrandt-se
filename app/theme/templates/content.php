c64f6bea1537d9f9b2115652a9fa87baf12a6e6e
Marie Hogebrandt <iam@mariehogebrandt.se>
2013-05-19 23:02:07 +0200

    Adjusting templates

--- app/theme/templates/content.php
+++ app/theme/templates/content.php
@@ -1,4 +1,4 @@
-<article <?php post_class(); ?>>
+<article <?php post_class('inner'); ?>>
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
--- app/theme/templates/page-header.php
+++ app/theme/templates/page-header.php
@@ -1,5 +1,5 @@
-<div class="page-header">
+<header class="page-header">
   <h1>
     <?php echo roots_title(); ?>
   </h1>
-</div>
+</header>
