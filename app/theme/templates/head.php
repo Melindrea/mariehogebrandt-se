be194ae1e90686200bb0f2a995353bb729667f91
Marie Hogebrandt <marie@pineberry.com>
2013-10-15 08:00:59 +0200

    Adding analytics to head

--- app/theme/templates/head.php
+++ app/theme/templates/head.php
@@ -12,4 +12,15 @@
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+  <!-- TODO: change this to for instance use functions.php -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-42118135-1', 'mariehogebrandt.se');
+    ga('send', 'pageview');
+
+  </script>
 </head>
